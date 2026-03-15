d42fdc7e58a5b9292f18e7a4835a2d64cf9922f4
Chang <cliu81530@gmail.com>
2026-03-15 22:24:42 +0100

    add getMax method

--- Program.cs
+++ Program.cs
@@ -11,3 +11,16 @@ double CalculateAverage(int[] array)
     double average = (double)sum / array.Length;
     return average;
 }
+
+static int getMax(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
