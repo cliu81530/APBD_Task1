18f096aa3fe938aba4c8e70eddafcc9221448a41
Chang <cliu81530@gmail.com>
2026-03-15 22:22:54 +0100

    add calculateAverate to program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
+
+
+double CalculateAverage(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    double average = (double)sum / array.Length;
+    return average;
+}
